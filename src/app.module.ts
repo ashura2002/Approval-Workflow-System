065a800ad18bb588185e956695ffd1f1fb7118f3
Jmark Dayna <jemarcdayna352@gmail.com>
2026-02-16 22:44:23 +0800

    added users, request modules and used prisma orm for database actions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { RequestsModule } from './modules/requests/requests.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, RequestsModule],
   controllers: [AppController],
   providers: [AppService],
 })
