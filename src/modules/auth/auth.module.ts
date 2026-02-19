0c525f0c6c4f4213c107b3a7f3fd3ec90cc5973f
Jmark Dayna <jemarcdayna352@gmail.com>
2026-02-19 23:10:00 +0800

    added register and login route on Auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/common/prisma.service';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { jwtConfigService } from 'src/config/jwt.config.service';
 
 @Module({
-  imports:[UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: jwtConfigService,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
   exports: [AuthService],
