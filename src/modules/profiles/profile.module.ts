5a146dc1cf3e7cee045f16384c7b12113562186a
Jmark Dayna <jemarcdayna352@gmail.com>
2026-03-06 22:44:00 +0800

    added create getbyId and getCurrent profile endpoints and services

--- src/modules/profiles/profile.module.ts
+++ src/modules/profiles/profile.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 import { PrismaService } from 'src/common/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [JwtModule],
   controllers: [ProfileController],
   providers: [ProfileService, PrismaService],
   exports: [ProfileService],
