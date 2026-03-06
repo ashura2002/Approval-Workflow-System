084ee59ec9d8a80775f47b0ce246b2e2edfdf19b
Jmark Dayna <jemarcdayna352@gmail.com>
2026-03-06 21:38:35 +0800

    added profile module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { RequestsModule } from './modules/requests/requests.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { CompanyModule } from './modules/company/company.module';
+import { ProfileModule } from './modules/profiles/profile.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { CompanyModule } from './modules/company/company.module';
     RequestsModule,
     CompanyModule,
     RequestsModule,
+    ProfileModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
